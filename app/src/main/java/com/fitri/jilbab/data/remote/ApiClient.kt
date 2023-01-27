a76e9d226e58ed9ee5f98fe1a29548d67a8748f8
nasya <nasya@xyber.id>
2023-01-27 17:55:46 +0700

    add apiclient

---
+++ app/src/main/java/com/fitri/jilbab/data/remote/ApiClient.kt
@@ -0,0 +1,39 @@
+package com.fitri.jilbab.data.remote
+
+import com.skydoves.sandwich.coroutines.CoroutinesResponseCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
+
+class ApiClient @Inject constructor(
+    private val okHttpClient: OkHttpClient
+) {
+
+    fun instance() : ApiServices {
+        val retrofit = Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("http://localhost:8000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutinesResponseCallAdapterFactory.create())
+            .build()
+
+        return retrofit.create(ApiServices::class.java)
+    }
+
+}
+
+fun okHttpClient(authInterceptor: AuthInterceptor): OkHttpClient =
+    OkHttpClient.Builder()
+        .addInterceptor(authInterceptor)
+        .addInterceptor(logging)
+        .build()
+
+private val logging: HttpLoggingInterceptor
+    get() {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        return httpLoggingInterceptor.apply {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
