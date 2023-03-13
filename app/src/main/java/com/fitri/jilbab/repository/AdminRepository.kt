88d22ec0543ac20e9faded0d544c32580838627c
nasya zafira <nasya.zafira16@gmail.com>
2023-03-13 13:10:26 +0700

    add product

--- app/src/main/java/com/fitri/jilbab/repository/AdminRepository.kt
+++ app/src/main/java/com/fitri/jilbab/repository/AdminRepository.kt
@@ -10,6 +10,9 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.Part
 import javax.inject.Inject
 
 class AdminRepository @Inject constructor(
@@ -35,7 +38,7 @@ class AdminRepository @Inject constructor(
         .onCompletion { onComplete() }
         .flowOn(ioDispatcher)
 
-       suspend fun categoryAdmin(
+    suspend fun categoryAdmin(
         onStart: () -> Unit,
         onComplete: () -> Unit,
         onError: (String?) -> Unit,
@@ -53,7 +56,50 @@ class AdminRepository @Inject constructor(
         .onCompletion { onComplete() }
         .flowOn(ioDispatcher)
 
-
+    suspend fun addProductAd(
+        onStart: () -> Unit,
+        onComplete: () -> Unit,
+        onError: (String?) -> Unit,
+        thumbnail: MultipartBody.Part,
+        productimage1: MultipartBody.Part,
+        productimage2: MultipartBody.Part,
+        productimage3: MultipartBody.Part,
+        productimage4: MultipartBody.Part,
+        nama_produk: RequestBody,
+        harga_produk: RequestBody,
+        diskon_produk: RequestBody,
+        kategori_produk: RequestBody,
+        berat_produk: RequestBody,
+        stock_default: RequestBody,
+        deskripsi_produk: RequestBody,
+        detail_info: RequestBody
+    ) = flow {
+        val response = apiService.addProduct(
+            thumbnail,
+            productimage1,
+            productimage2,
+            productimage3,
+            productimage4,
+            nama_produk,
+            harga_produk,
+            diskon_produk,
+            kategori_produk,
+            berat_produk,
+            stock_default,
+            deskripsi_produk,
+            detail_info
+        )
+        response.suspendOnSuccess {
+            emit(data)
+        }.onError {
+            onError(this.message())
+        }.onException {
+            onError(this.message())
+        }
+    }
+        .onStart { onStart() }
+        .onCompletion { onComplete() }
+        .flowOn(ioDispatcher)
 
 
 }
