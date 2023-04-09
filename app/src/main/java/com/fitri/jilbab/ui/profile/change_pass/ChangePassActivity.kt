78156aeb4f2a432b579ab642005801e14a55db74
nasya zafira <nasya.zafira16@gmail.com>
2023-04-09 21:24:20 +0700

    add change password

---
+++ app/src/main/java/com/fitri/jilbab/ui/profile/change_pass/ChangePassActivity.kt
@@ -0,0 +1,11 @@
+package com.fitri.jilbab.ui.profile.change_pass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_pass)
+    }
+}
